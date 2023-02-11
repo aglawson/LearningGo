10cd88d9ef778456af4dd97226f01628dbaaecb8
Adrian Lawson <adrianglawson@gmail.com>
2023-02-11 11:43:01 -0800

    organize functions outside of main

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/big"
 	"net/http"
 	"strconv"
 
@@ -15,12 +16,6 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	// Age  int    `json:"age"`
-}
-
 type Response struct {
 	Success bool   `json:"success"`
 	Data    string `json:"data"`
@@ -28,36 +23,61 @@ type Response struct {
 
 var res = Response{Success: true, Data: "wut"}
 
-const KEY = "<YOUR-KEY-HERE>"
-
-var users = []User{
-	{ID: 1, Name: "John Doe"},
-	{ID: 2, Name: "Jane Doe"},
-}
+const KEY = "<ALCHEMY-KEY>"
 
 // Connect to an Ethereum node
 var client, err = ethclient.Dial("https://eth-mainnet.g.alchemy.com/v2/" + KEY)
 
-func main() {
-	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		queryParams := r.URL.Query()
-		name := queryParams.Get("name")
+type Result struct {
+	PrivateKey string
+	Address    string
+}
 
-		i := 0
-		for i < len(users) {
-			if name == users[i].Name {
-				res.Success = true
-				res.Data = name + " " + strconv.Itoa(users[i].ID)
-				json.NewEncoder(w).Encode(res)
-				return
-			}
-			i += 1
-		}
-		res.Success = false
-		res.Data = "Not Found"
-		json.NewEncoder(w).Encode(res)
-	})
+func createWallet() Result {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	//fmt.Println(hexutil.Encode(privateKeyBytes)[2:])
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	// publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	result := Result{PrivateKey: hexutil.Encode(privateKeyBytes)[2:], Address: address}
+	return result
+}
+
+func getBalance(address string) *big.Int {
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	blockNumber, err := client.BlockByNumber(context.Background(), nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	// Retrieve the latest block number
+	balance, err := client.BalanceAt(context.Background(), common.HexToAddress(address), blockNumber.Number())
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	// Retrieve the latest block number
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	return balance
+}
+
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(res)
@@ -79,50 +99,18 @@ func main() {
 	http.HandleFunc("/getBalance", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		queryParams := r.URL.Query()
+
 		address := queryParams.Get("wallet")
-		blockNumber, err := client.BlockByNumber(context.Background(), nil)
-		if err != nil {
-			fmt.Println("Error: ", err)
-		}
-		// Retrieve the latest block number
-		balance, err := client.BalanceAt(context.Background(), common.HexToAddress(address), blockNumber.Number())
-		if err != nil {
-			fmt.Println("Error: ", err)
-		}
-		// fmt.Println(blockNumber.NumberU64())
-		//res.Data = strconv.Itoa(balance)
+
+		balance := getBalance(address)
+
 		json.NewEncoder(w).Encode(balance)
 	})
 
 	http.HandleFunc("/createWallet", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
-		privateKey, err := crypto.GenerateKey()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		privateKeyBytes := crypto.FromECDSA(privateKey)
-		//fmt.Println(hexutil.Encode(privateKeyBytes)[2:])
-
-		publicKey := privateKey.Public()
-		publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-		// publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
-		address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
-
-		if !ok {
-			log.Fatal("error casting public key to ECDSA")
-		}
-
-		type Result struct {
-			PrivateKey string
-			Address    string
-		}
-		result := Result{PrivateKey: hexutil.Encode(privateKeyBytes)[2:], Address: address}
-
-		// hash := sha3.NewKeccak256()
-		// hash.Write(publicKeyBytes[1:])
-		// fmt.Println(hexutil.Encode(hash.Sum(nil)[12:]))
+		result := createWallet()
 
 		json.NewEncoder(w).Encode(result)
 	})
