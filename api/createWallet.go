d3072e1f1db331fc6df11f23f8a03ccc08662940
Adrian Lawson <adrianglawson@gmail.com>
2023-02-11 11:50:28 -0800

    organize api

---
+++ api/createWallet.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+type Result struct {
+	PrivateKey string
+	Address    string
+}
+
+func createWallet() Result {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	//fmt.Println(hexutil.Encode(privateKeyBytes)[2:])
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	// publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	result := Result{PrivateKey: hexutil.Encode(privateKeyBytes)[2:], Address: address}
+	return result
+}
