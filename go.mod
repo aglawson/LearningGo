74dfcc2c7f8040f2f220713aa05b328a49174e39
Adrian Lawson <adrianglawson@gmail.com>
2023-02-10 22:57:38 -0800

    get latest block

--- go.mod
+++ go.mod
@@ -5,6 +5,19 @@ go 1.20
 require rsc.io/quote v1.5.2
 
 require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/ethereum/go-ethereum v1.10.26 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.5 // indirect
+	github.com/tklauser/numcpus v0.2.2 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	rsc.io/sampler v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
+
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 type User struct {
@@ -17,7 +21,7 @@ type Response struct {
 	Data    string `json:"data"`
 }
 
-var res = Response{Success: true, Data: "success"}
+var res = Response{Success: true, Data: "wut"}
 
 var users = []User{
 	{ID: 1, Name: "John Doe"},
@@ -46,6 +50,23 @@ func main() {
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(res)
+	})
+
+	http.HandleFunc("/getBlock", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		// Connect to an Ethereum node
+		client, err := ethclient.Dial("https://eth-mainnet.g.alchemy.com/v2/<YOUR-KEY-HERE>")
+		if err != nil {
+			fmt.Println("Error connecting to Ethereum node:", err)
+			return
+		}
+
+		// Retrieve the latest block number
+		blockNumber, err := client.BlockByNumber(context.Background(), nil)
+		// fmt.Println(blockNumber.NumberU64())
+		res.Data = strconv.Itoa(int(blockNumber.NumberU64()))
 		json.NewEncoder(w).Encode(res)
 	})
 
