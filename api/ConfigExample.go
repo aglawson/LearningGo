a63cbcccfb3676764a657beda7b007bedcbc36f8
Adrian Lawson <adrianglawson@gmail.com>
2023-02-13 17:34:19 -0800

    add example config file

---
+++ api/ConfigExample.go
@@ -0,0 +1,14 @@
+package api
+
+// type Config struct {
+// 	PK         string
+// 	INFURA_KEY string
+// }
+
+//@TO-DO: uncomment code and enter your keys
+// 		  change file name to 'Config.go'
+
+// var Conf = Config {
+// 	PK: "<YOUR-PRIVATE-KEY>", // only needed for sending state-changing txs, not for reading data from a contract. make sure to protect you private key and don't push it to github
+// 	INFURA_KEY: "<YOUR-INFURA-KEY>",
+// }
