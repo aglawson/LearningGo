bed78e268f73b28ab8797a1ce83abf5281a23ab0
Adrian Lawson <adrianglawson@gmail.com>
2023-02-27 13:28:33 -0800

    write price data to Firestore

--- api/GetCoinPrice.go
+++ api/GetCoinPrice.go
@@ -4,19 +4,39 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+
+	"context"
+
+	firebase "firebase.google.com/go"
 )
 
-type CoinGeckoResponse struct {
-	MarketData struct {
-		CurrentPrice map[string]float64 `json:"current_price"`
-	} `json:"market_data"`
-}
-
 func GetCoinPrice(from string, to string) (float64, error) {
+	// Use the application default credentials
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: Conf.FirebaseProjectId}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
 
 	if from == "polygon" || from == "matic" {
 		from = "wmatic"
+	}
+
+	if from == "eth" {
+		from = "ethereum"
+	}
+
+	if from == "btc" {
+		from = "bitcoin"
 	}
 
 	url := "https://api.coingecko.com/api/v3/simple/price?ids=" + from + "&vs_currencies=" + to //"https://api.coingecko.com/api/v3/simple/price?ids=" + name + "&vs_currencies=usd"
@@ -47,6 +67,14 @@ func GetCoinPrice(from string, to string) (float64, error) {
 	answer := data[from][to]
 
 	fmt.Println(answer)
+	_, _, err = client.Collection("prices").Add(ctx, map[string]interface{}{
+		"from":  from,
+		"to":    to,
+		"value": answer,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding aturing: %v", err)
+	}
 
 	return answer, nil
 }
